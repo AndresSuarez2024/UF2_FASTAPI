58d000fc416dfba07b5d4373f15e3715fa8c0e1d
andres <2024_andres.suarez@iticbcn.cat>
2025-03-02 20:23:47 +0100

    Eliminada la carpeta actividad 11 de actividad 12

--- ACTIVITAT_11/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'  # El teu usuari de MySQL
-    MYSQL_PASSWORD = 'Coco120604@'  # La teva contrasenya de MySQL
-    MYSQL_DB = 'penjat'  # El nom de la base de dades
